939ba0aba50bc0644d567140ca1bb33c6a55ed02
Ryan Sundberg <ryan.sundberg@gmail.com>
2019-07-11 10:27:37 -0700

    Secret key generation & loading

--- src/arctype/service/jose.clj
+++ src/arctype/service/jose.clj
@@ -1,4 +1,8 @@
 (ns arctype.service.jose
+  (:import 
+    [javax.crypto KeyGenerator SecretKey]
+    [javax.crypto.spec SecretKeySpec]
+    [org.apache.commons.codec.binary Hex])
   (:require
     [clojure.tools.logging :as log]
     [arctype.service.protocol :refer :all]
@@ -6,28 +10,50 @@
     [sundbry.resource :as resource]))
 
 (def Config
-  {:keys {S/Keyword S/Str}})
+  {:default-key S/Str
+   :keys {S/Keyword S/Str}})
 
 (def ^:private default-config
   {})
 
-(defrecord JoseService []
+(S/defn generate-key :- S/Str
+  []
+  (let [keygen (KeyGenerator/getInstance "AES")]
+    (.init keygen 128)
+    (let [sk (.generateKey keygen)]
+      (Hex/encodeHexString (.getEncoded sk)))))
+
+(S/defn encrypt :- S/Str
+  [body :- S/Str]
+  (throw (UnsupportedOperationException. "TODO encrypt")))
+
+(defrecord JoseService [default-key secret-keys]
   PLifecycle
 
   (start [this]
-    (log/info {:message "Starting JOSE service"})
-    )
+    (log/info {:message "Starting JOSE service" :default-key default-key})
+    this)
 
   (stop [this]
     (log/info {:message "Stopping JOSE service"})
-    )
+    this)
 
   )
+
+(defn- load-secret-key
+  [^String hex-str]
+  (let [sk-bytes (.decode (Hex.) (.getBytes hex-str))]
+    (SecretKeySpec. sk-bytes "AES")))
 
 (S/defn create
   [resource-name
    config :- Config]
-  (let [config (merge default-config config)]
+  (let [config (merge default-config config)
+        secret-keys (->> (:keys config)
+                         (map (fn [[key-id key-str]]
+                                [(name key-id) (load-secret-key key-str)]))
+                         (into {}))]
     (resource/make-resource
-      (map->JoseService {})
+      (map->JoseService {:default-key (:default-key config)
+                         :secret-keys secret-keys})
       resource-name)))
